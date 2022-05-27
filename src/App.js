cec25d599378021b26c22182387c57a193804122
Shashi Lata <100056409+shashilata9519@users.noreply.github.com>
2022-05-27 10:23:31 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+
+const Game = (props) => {
+  let x = props.x;
+  let y = props.y;
+  let z = props.z;
+  if (x === y && y === z) {
+    return (
+      <>
+        <div className="main">
+          <h1 >
+            {x} {y} {z}
+          </h1>
+          <h1 style={{ color: "green" }}>match</h1>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div className="main">
+          <h1>
+            {x} {y} {z}
+          </h1>
+          <h1 style={{ color: "red" }}> not match</h1>
+        </div>
+      </>
+    );
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="head-section">
+        🎰<span>slot machine game</span>🎰
+      </h1>
+      <div className="center">
+        <Game x="😎" y="😎" z="😎" />
+        <Game x="🎈" y="🎗" z="🎁" />
+        <Game x="🎈" y="🎁" z="🎁" />
+      </div>
+    </>
   );
 }
 
